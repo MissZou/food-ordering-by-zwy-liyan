2660fa31acb7d988608ebd7195a98cd6589a0ac3
MoonSlides <461350652@qq.com>
2016-01-18 17:00:29 +0800

    delete thigs

--- food-ordering/server.js
+++ food-ordering/server.js
@@ -122,7 +122,7 @@ router.route('/resetPassword')
 	
 	.get(function(req, res){
 		var accountId = req.param('account', null);
-  		res.render('resetPassword.jade', {accountId:accountId});
+  		res.render('resetPassword.jade', {accountId:accountId});// delete local.
 	})
 
 	.post(function(req, res){
